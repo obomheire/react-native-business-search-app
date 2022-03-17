be223c6a13d0f8183777653d19724cfbec33e8e8
obomheire <obomheire@gmail.com>
2022-03-17 15:24:53 +0100

    track app started

---
+++ react-native-track-app/track-client/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://e58a-154-118-56-149.ngrok.io",
+});
---
+++ react-native-track-app/track-client/src/context/AuthContext.js
@@ -0,0 +1,40 @@
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const signup = dispatch => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    // somehow sign out!!!
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signout, signup },
+  { isSignedIn: false }
+);
